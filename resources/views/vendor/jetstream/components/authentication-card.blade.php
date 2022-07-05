19b04ee4b61d41a15b3458d68595abbbc60fe3ad
Fapcod <frankantoniopiato@gmail.com>
2022-07-04 22:55:48 -0500

    actualizacion

---
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -0,0 +1,9 @@
+<div class="min-h-screen md:flex md:justify-center md:items-center pt-6 sm:pt-6 bg-cover" style="background-image: url(img/bgg.png)">
+    <div class="pt-8">
+        {{ $logo }}
+    </div>
+
+    <div class="w-full md:max-w-md mt-6 px-6 py-4 bg-white shadow-2xl overflow-hidden rounded-lg ">
+        {{ $slot }}
+    </div>
+</div>
